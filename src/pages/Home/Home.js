8b51e31a6c5df5e4a7fd4155a6cd73b14131f14d
Divya70 <divyanamdev8795@gmail.com>
2022-10-14 15:38:05 +0530

    feat: created home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.scss";
+import { Navbar } from "../../components/Navbar/Navbar";
+const Home = () => {
+  return (
+    <div className="homepage-main-sec">
+      <Navbar />
+    </div>
+  );
+};
+
+export { Home };
